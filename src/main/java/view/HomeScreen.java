0b2a913751ea6553f5d306dcca7d7b6c0164276f
LiGreT <ligret.regret@gmail.com>
2026-04-27 23:35:54 +0900

    ホーム画面を作成 #2

--- src/main/java/view/HomeScreen.java
+++ src/main/java/view/HomeScreen.java
@@ -1,7 +1,6 @@
 package main.java.view;
 
-import java.awt.Component;
-
+import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -10,11 +9,35 @@ import javax.swing.*;
  */
 public class HomeScreen extends JPanel {
     public HomeScreen() {
+        /* パネル全体の設定 */
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setPreferredSize(new Dimension(1080, 720));
 
+        /* タイトルの設定 */
         JLabel titleLabel = new JLabel("例文で覚える単語帳アプリ");
+        titleLabel.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 40));
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        /* ボタンを入れるパネル */
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        GridBagConstraints btnPnlBagConstraints = new GridBagConstraints();
+        btnPnlBagConstraints.gridx = 0;
+        btnPnlBagConstraints.gridy = GridBagConstraints.RELATIVE;
+        btnPnlBagConstraints.insets = new Insets(10, 0, 10, 0);
+
+        /* 「学習を始める」ボタンの設定 */
+        JButton startButton = new JButton("学習を始める");
+        startButton.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 35));
+        startButton.setPreferredSize(new Dimension(360, 120));
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        /* ボタンをパネルに追加 */
+        buttonPanel.add(startButton, btnPnlBagConstraints);
+
+        /* パネルに要素を追加 */
         this.add(titleLabel);
+        this.add(buttonPanel);
     }
 }
