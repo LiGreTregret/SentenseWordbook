07c0a45e8bda53d01655726f55bf2e037bd81682
LiGreT <ligret.regret@gmail.com>
2026-05-08 16:42:57 +0900

    ParentFrameクラスとそのテストを実装 #4

---
+++ src/main/java/view/ParentFrame.java
@@ -0,0 +1,35 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ParentFrame extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    public ParentFrame() {
+        /* 基本設定 */
+        setTitle("例文で覚える単語帳");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1080, 720);
+
+        /* 画面遷移を行えるパネルの設定 */
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        /* 各画面のインスタンス化 */
+        // HomeScreen homeScreen = new HomeScreen(this);
+        // StudyScreen studyScreen = new StudyScreen(this);
+
+        /* パネルに画面を登録 */
+        // mainPanel.add(homeScreen, "Home");
+        // mainPanel.add(studyScreen, "Study");
+        
+        add(mainPanel);
+        setVisible(true);
+    }
+
+    public void showScreen(String screenName) {
+        cardLayout.show(mainPanel, screenName);
+    }
+}
---
+++ src/test/java/view/ParentFrameTest.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class ParentFrameTest {
+    public static void main(String[] args) {
+        ParentFrame parentFrame = new ParentFrame();
+        parentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        parentFrame.setVisible(true);
+    }
+}
